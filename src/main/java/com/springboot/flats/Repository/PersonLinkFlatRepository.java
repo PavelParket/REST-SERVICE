8e51a7fc8e97909faaf9ef395f814bc71262fd3a
PavelParket <124335769+PavelParket@users.noreply.github.com>
2024-05-20 01:02:39 +0300

    Delete src directory

--- src/main/java/com/springboot/flats/Repository/PersonLinkFlatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.flats.Repository;
-
-import com.springboot.flats.Entity.PersonLinkFlat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonLinkFlatRepository extends JpaRepository<PersonLinkFlat, Long> {
-}
--- src/main/java/com/springboot/flats/Repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.flats.Repository;
-
-import com.springboot.flats.Entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
