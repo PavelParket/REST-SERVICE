e2ee709631cd971e1e64ad69b7a574ae647f0abf
PavelParket <124335769+PavelParket@users.noreply.github.com>
2024-05-20 01:00:39 +0300

    Delete flats/Repository directory

--- flats/Repository/FlatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.flats.Repository;
-
-import com.springboot.flats.Entity.Flat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlatRepository extends JpaRepository<Flat, Long> {
-}
